8a943637f6469d194661a77cc77311dff4516498
suichanwa <augustinnegura746@gmail.com>
2026-03-26 12:36:00 +0200

    change mod version so it'll work on the server

--- java/com/overlast/OverLast.java
+++ java/com/overlast/OverLast.java
@@ -21,7 +21,7 @@ public class OverLast {
 	// Basic mod constants.
 	public static final String MOD_ID = "overlast";
 	public static final String MOD_NAME = "OverLast";
-	public static final String VERSION = "0.1.9";
+	public static final String VERSION = "0.2.9";
 	public static final String MCVERSION = "1.12";
 	public static final String DEPENDENCIES = "required-after:forge@[14.23.1.2611,);required-after:srparasites";
 	public static final String RESOURCE_PREFIX = MOD_ID + ":"; // overlast:
